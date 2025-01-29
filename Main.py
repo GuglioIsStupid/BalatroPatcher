97b24d6307a9d8a54cba5ca31164a9b2a5abf539
GuglioIsStupid <bradsbluetab@gmail.com>
2025-01-29 13:50:18 -0500

    Create patcher

---
+++ Main.py
@@ -0,0 +1,21 @@
+import util.SteamPatcher as SteamPatcher
+import sys, os
+
+def main(argv):
+    if len(argv) < 2:
+        print("Usage: python main.py <patchType (steam|apk)>")
+        return
+    
+    patchType = argv[1].lower()
+
+    if patchType == "steam":
+        SteamPatcher.Patch()
+    elif patchType == "apk":
+        if len(argv) < 3:
+            print("Usage: python main.py apk <apkPath>")
+            return
+        
+        apkPath = argv[2]
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ util/SteamPatcher.py
@@ -0,0 +1,107 @@
+# libs to get Balatro install path from steam
+import os, sys, shutil
+import json
+import winreg
+import zipfile
+
+gameID: str = "2379780"
+
+luaAdditionStart = """
+-- call love function that loads file from save folder first
+local oldLoad = love.filesystem.load
+love.filesystem.load = function(path)
+    if love.filesystem.getInfo("Patched/" .. path) then
+		return oldLoad("Patched/" .. path)
+	end
+    
+    if love.filesystem.getInfo(path) then
+        return oldLoad(path)
+    end
+
+    return nil
+end
+
+lovely = require("smods.lovely")
+
+"""
+
+luaAdditionEnd = """
+local function loadMods()
+    require("smods.src.core")
+
+    if not love.filesystem.getInfo("Mods") then
+        love.filesystem.createDirectory("Mods")
+    end
+end
+
+loadMods()
+"""
+
+def Patch():
+    # get steam install path for Balatro
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Valve\Steam")
+        steamPath = winreg.QueryValueEx(key, "SteamPath")[0]
+        winreg.CloseKey(key)
+    except:
+        print("Steam not found in registry")
+        return
+    
+    print("Steam found at: " + steamPath)
+
+    # get Balatro install path from registry
+    try:
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App " + gameID)
+        balatroPath = winreg.QueryValueEx(key, "InstallLocation")[0]
+        winreg.CloseKey(key)
+    except:
+        print("Balatro not found in registry")
+        return
+    
+    print("Balatro found in registry at: " + balatroPath)
+
+    # extract Balatro.exe
+    try:
+        # create folder "lua" and extract Balatro.exe
+        os.makedirs(balatroPath + "\\lua", exist_ok=True)
+        with zipfile.ZipFile(balatroPath + "\\Balatro.exe", 'r') as zip_ref:
+            zip_ref.extractall(balatroPath + "\\lua")
+    except:
+        print("Failed to extract Balatro.exe")
+        return
+    
+    mainLuaPath = balatroPath + "\\lua\\main.lua"
+    # read main.lua
+    try:
+        with open(mainLuaPath, 'r') as file:
+            mainLua = file.read()
+    except:
+        print("Failed to read main.lua")
+        return
+    
+    mainLua = luaAdditionStart + mainLua + luaAdditionEnd
+
+    # write main.lua
+    try:
+        with open(mainLuaPath, 'w') as file:
+            file.write(mainLua)
+    except:
+        print("Failed to write main.lua")
+        return
+    
+    # copy smods/ folder to lua folder
+    try:
+        shutil.copytree("smods", balatroPath + "\\lua\\smods")
+    except:
+        print("Failed to copy smods/ folder")
+        return
+    
+    # zip lua folder to Balatro.love
+    try:
+        with zipfile.ZipFile(balatroPath + "\\Balatro.love", 'w') as zip_ref:
+            for root, dirs, files in os.walk(balatroPath + "\\lua"):
+                for file in files:
+                    zip_ref.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(balatroPath, "lua")))
+    except:
+        print("Failed to zip lua folder")
+        return
